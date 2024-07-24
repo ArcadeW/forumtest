4a93b5e7180886d4561906d05574350c89dd4684
ArcadeW <108231469+ArcadeW@users.noreply.github.com>
2024-07-24 14:30:00 +0200

    Add files via upload

---
+++ conf_global.php
@@ -0,0 +1,18 @@
+<?php
+
+$INFO = array (
+  'sql_host' => 'localhost',
+  'sql_database' => 'www10717_testoweforumcyber',
+  'sql_user' => 'www10717_testoweforumcyber',
+  'sql_pass' => 'GqD9B4aT6iaKvM3bAwaK',
+  'sql_port' => 3306,
+  'sql_socket' => '/var/run/mysqld/mysqld.sock',
+  'sql_tbl_prefix' => '',
+  'sql_utf8mb4' => true,
+  'board_start' => 1721815366,
+  'installed' => true,
+  'base_url' => 'http://site30669.web1.titanaxe.com/',
+  'guest_group' => 2,
+  'member_group' => 3,
+  'admin_group' => 4,
+);
